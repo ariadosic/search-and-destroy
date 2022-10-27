709087ad3c03da8fb57367968ff76e4786edce06
Alex Sun <alexsun424@gmail.com>
2022-10-27 18:01:52 -0400

    finished everything except last extra credit

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,62 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+const binarySearch = (arr, x) => {
+	// while(array.length > 0){
+	// 	const root = Math.floor((array.length - 1) / 2)
+	// 	console.log(array, "root is: ", root)
+	// 	if(target < array[root]){
+	// 		const leftHalf = array.slice(0, root)
+	// 		array = leftHalf
+	// 	} else if (target > array[root]) {
+	// 		const rightHalf = array.slice(root + 1)
+	// 		array = rightHalf
+	// 	} else {
+	// 		return true;
+	// 	}
+	// }
+
+	// return false
+
+
+	// let leftPointer = 0
+	// let rightPointer = array.length - 1
+
+	// while(true){
+	// 	const root = leftPointer + Math.floor((rightPointer - leftPointer) / 2)
+	// 	console.log("L", leftPointer, "R", rightPointer, "RT", root)
+	// 	if (array[root] === target || array[root - 1] === target || array[root + 1] === target){
+	// 		return true;
+	// 	} else if (rightPointer - leftPointer === 1){
+	// 		break;
+	// 	} else if (target < array[root]){
+	// 		rightPointer = root
+	// 	} else if (target > array[root]) {
+	// 		leftPointer = root
+	// 	} 
+	// }
+	// console.log("BROKE")
+	// return false
+
+	let start=0, end=arr.length-1;
+         
+    // Iterate while start not meets end
+    while (start<=end){
+ 
+        // Find the mid index
+        let mid=Math.floor((start + end)/2);
+  
+        // If element is present at mid, return True
+        if (arr[mid]===x) return true;
+ 
+        // Else look in left or right half accordingly
+        else if (arr[mid] < x)
+             start = mid + 1;
+        else
+             end = mid - 1;
+    }
+  
+    return false;
 };
 
 /*
--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,27 @@
 
 // Complete this algo
 const minJumps = arr => {
+    let position = 0 //index
+    let array = []
+    let jumpCount = 0
 
+    while(position < arr.length - 1){
+        const jumpNumber = arr[position] //actual value
+        if(position + jumpNumber >= arr.length - 1){
+            return ++jumpCount
+        }
+        
+        for(let i = 1; i <= jumpNumber; i++){
+            array.push(arr[position + i]) //actual value
+        }
+        let maxValue = Math.max(...array)
+        let jump = array.indexOf(maxValue) + 1
+        array = [];
+        position += jump 
+       
+        jumpCount++
+    }
+    return jumpCount
 };
 
 module.exports = minJumps
